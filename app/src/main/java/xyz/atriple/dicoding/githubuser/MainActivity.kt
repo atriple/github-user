d0f99f6c5cd8d0c2c1114c2c3361d776745e8981
Andi Aqil A <40452321+atriple@users.noreply.github.com>
2020-08-28 22:56:54 +0700

    Add `RecycleView` to gradle, use android extensions

--- app/src/main/java/xyz/atriple/dicoding/githubuser/MainActivity.kt
+++ app/src/main/java/xyz/atriple/dicoding/githubuser/MainActivity.kt
@@ -2,6 +2,7 @@ package xyz.atriple.dicoding.githubuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
