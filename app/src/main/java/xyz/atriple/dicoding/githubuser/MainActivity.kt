e8e3c5e757f2bebc67c318a4b3d61d06cb271dc9
Andi Aqil A <40452321+atriple@users.noreply.github.com>
2020-09-02 12:56:25 +0700

    Fix the avatar not showing due to bad values

--- app/src/main/java/xyz/atriple/dicoding/githubuser/MainActivity.kt
+++ app/src/main/java/xyz/atriple/dicoding/githubuser/MainActivity.kt
@@ -2,6 +2,7 @@ package xyz.atriple.dicoding.githubuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import xyz.atriple.dicoding.githubuser.adapter.UserAdapter
 import xyz.atriple.dicoding.githubuser.model.User
@@ -15,12 +16,14 @@ class MainActivity : AppCompatActivity() {
 
         users = createUserList()
 
-        //rv_github_user.adapter =
+        val adapter = UserAdapter(users)
+        rv_github_user.adapter = adapter
+        rv_github_user.layoutManager = LinearLayoutManager(this)
     }
 
-    fun createUserList(): ArrayList<User>{
+    fun createUserList(): ArrayList<User> {
         //TODO: This can be optimized (probably?)
-        val users : ArrayList<User> = ArrayList<User>()
+        val users: ArrayList<User> = ArrayList()
         val usernames = resources.getStringArray(R.array.username)
         val names = resources.getStringArray(R.array.name)
         val locations = resources.getStringArray(R.array.location)
@@ -28,9 +31,11 @@ class MainActivity : AppCompatActivity() {
         val companies = resources.getStringArray(R.array.company)
         val followers = resources.getStringArray(R.array.followers)
         val following = resources.getStringArray(R.array.following)
-        val avatars = resources.getStringArray(R.array.avatar)
+        val avatars = resources.getStringArray(R.array.avatar).map{
+            it.removePrefix("res/").removeSuffix(".png")
+        }
 
-        for((index, _) in usernames.withIndex()){
+        for ((index, _) in usernames.withIndex()) {
             users.add(
                 User(
                     usernames[index],
@@ -40,8 +45,11 @@ class MainActivity : AppCompatActivity() {
                     companies[index],
                     followers[index],
                     following[index],
-                    resources.getIdentifier(avatars[index], null, packageName)
-                ))
+                    resources.getIdentifier(
+                        avatars[index], null, packageName
+                    )
+                )
+            )
         }
 
         return users
--- app/src/main/java/xyz/atriple/dicoding/githubuser/adapter/UserAdapter.kt
+++ app/src/main/java/xyz/atriple/dicoding/githubuser/adapter/UserAdapter.kt
@@ -4,6 +4,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import xyz.atriple.dicoding.githubuser.model.User
 import kotlinx.android.synthetic.main.item_user.view.*
 import xyz.atriple.dicoding.githubuser.R
@@ -23,6 +25,10 @@ class UserAdapter(private val mUser: List<User>) : RecyclerView.Adapter<UserAdap
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val user: User = mUser[position]
+
+        Glide.with(holder.itemView.context).load(user.avatar).apply(RequestOptions())
+            .override(60, 60).into(holder.itemView.img_item_user)
+
         holder.itemView.tv_item_name.text = user.name
         holder.itemView.tv_item_username.text = user.username
         holder.itemView.tv_item_location.text = user.location
